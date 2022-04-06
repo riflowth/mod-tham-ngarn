89db8312f83b46d904d99a92c61d5d3574a3d240
riflowth <riflowth@gmail.com>
2022-04-07 02:27:42 +0700

    feat: initial commit

---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <h1 className="text-2xl font-bold text-white">Mod Tham Ngarn</h1>  
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
