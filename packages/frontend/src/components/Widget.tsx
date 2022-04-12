24d2742186a79a3699691c211fdb8161fee705cb
Porping <porsteam@hotmail.com>
2022-04-12 19:49:06 +0700

    feat:add searchbar

--- packages/frontend/src/components/Widget.tsx
+++ packages/frontend/src/components/Widget.tsx
@@ -1,8 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
+import { BellIcon, PencilAltIcon } from "@heroicons/react/solid";
 import React from "react";
 
 const Widget = () => {
-  return <div className="flex-[6] bg-[#353147] "></div>;
+  return (
+    <div className="flex-[6] bg-[#353147] flex items-center sm:justify-between">
+      {/*Left item*/}
+
+      <div className="px-1">
+        <input
+          type="text"
+          className="w-32 h-8 px-2 rounded-xl "
+          placeholder="Search"
+        />
+      </div>
+
+      {/*  Right item*/}
+      <div className="flex justify-between px-6 space-x-2 md:w-2xl">
+        <PencilAltIcon className="w-10 h-10 text-white" />
+        <BellIcon className="w-10 h-10 text-white" />
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
