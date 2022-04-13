d506f852e04bc198d36a78341f9d649dba24974b
eltfshr <puntf2@gmail.com>
2022-04-13 22:28:33 +0700

    feat: adds Address, Branch model

---
+++ packages/backend/src/repositories/address/Address.ts
@@ -0,0 +1,31 @@
+export class Address {
+  private readonly postalCode: string;
+  private region: string;
+  private country: string;
+
+  public constructor(postalCode: string) {
+    this.postalCode = postalCode;
+  }
+
+  public getPostalCode(): string {
+    return this.postalCode;
+  }
+
+  public getRegion(): string {
+    return this.region;
+  }
+
+  public getCountry(): string {
+    return this.country;
+  }
+
+  public setRegion(region: string): Address {
+    this.region = region;
+    return this;
+  }
+
+  public setCountry(country: string): Address {
+    this.country = country;
+    return this;
+  }
+}
---
+++ packages/backend/src/repositories/branch/Branch.ts
@@ -0,0 +1,42 @@
+export default class Branch {
+  private readonly branchId: number;
+  private address: string;
+  private postalCode: string;
+  private telNo: string;
+
+  public constructor(branchId: number) {
+    this.branchId = branchId;
+  }
+
+  public getBranchId(): number {
+    return this.branchId;
+  }
+
+  public getAddress(): string {
+    return this.address;
+  }
+
+  public getPostalCode(): string {
+    return this.postalCode;
+  }
+
+  public getTelNo(): string {
+    return this.telNo;
+  }
+
+  public setAddress(address: string): Branch {
+    this.address = address;
+    return this
+  }
+
+  public setPostalCode(postalCode: string): Branch {
+    this.postalCode = postalCode;
+    return this
+  }
+
+  public setTelNo(telNo: string): Branch {
+    this.telNo = telNo;
+    return this
+  }
+}
+
