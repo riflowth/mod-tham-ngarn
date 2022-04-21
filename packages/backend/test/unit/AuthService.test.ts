81e2fa7bd80bafe7f6a52140bb3726819f3f960d
riflowth <riflowth@gmail.com>
2022-04-22 00:20:42 +0700

    feat: add auth service unit test

---
+++ packages/backend/test/unit/AuthService.test.ts
@@ -0,0 +1,29 @@
+import { LoginException } from '@/exceptions/service/LoginException';
+import { AuthService } from '@/services/AuthService';
+
+describe('Test implementation of Auth Service', () => {
+  const authService = new AuthService();
+
+  const validUsername = '123456789';
+  const validPassword = 'this_is_valid_password';
+  const invalidUsername = 'this_is_invalid_username';
+  const invalidPassword = 'short';
+
+  it('should throw LoginException with username requirement not match', async () => {
+    await expect(authService.login(invalidUsername, validPassword))
+      .rejects
+      .toThrow(LoginException);
+  });
+
+  it('should throw LoginException with password requirement not match', async () => {
+    await expect(authService.login(validUsername, invalidPassword))
+      .rejects
+      .toThrow(LoginException);
+  });
+
+  it('should not throw LoginException', async () => {
+    await expect(authService.login(validUsername, validPassword))
+      .resolves
+      .toBe(true);
+  });
+});
