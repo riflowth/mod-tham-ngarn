f37670e7920e5aebd9bd1f09fcb90561042db2fc
riflowth <riflowth@gmail.com>
2022-04-23 20:34:40 +0700

    refactor: add sql builder for utility

--- packages/backend/src/utils/database/Database.ts
+++ packages/backend/src/utils/database/Database.ts
@@ -1,14 +1,17 @@
 import { MySqlClient } from '@/utils/database/connectors/MySqlConnector';
 import { RedisClient } from '@/utils/database/connectors/RedisConnector';
+import { SqlBuilder } from '@/utils/SqlBuilder';
 
 export class Database {
 
   private readonly defaultDatabase: MySqlClient;
   private readonly cachingDatabase: RedisClient;
+  private readonly sqlBuilder: SqlBuilder;
 
   public constructor(defaultDatabase: MySqlClient, cachingDatabase: RedisClient) {
     this.defaultDatabase = defaultDatabase;
     this.cachingDatabase = cachingDatabase;
+    this.sqlBuilder = new SqlBuilder(defaultDatabase);
   }
 
   public query(sql: string, values: any) {
@@ -17,6 +20,10 @@ export class Database {
 
   public execute(sql: string, values: any) {
     return this.defaultDatabase.execute(sql, values);
+  }
+
+  public getSqlBuilder(): SqlBuilder {
+    return this.sqlBuilder;
   }
 
 }
