5a2fb4daf17ddd41d7eb44cbb0bf4411f32d49ab
XiaoXuxxxx <aonrok555@gmail.com>
2022-04-23 16:09:00 +0700

    feat: add machinepart repository

---
+++ packages/backend/src/repositories/machinePart/MachinePartRepository.ts
@@ -0,0 +1,6 @@
+import { MachinePart } from '@/entities/MachinePart';
+import { Repository } from '@/repositories/Repository';
+
+export interface MachinePartRepository extends Repository<MachinePart> {
+
+}
