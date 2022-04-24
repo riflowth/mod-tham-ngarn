cb1005b258cd053ad0998e25ca8114ab0f11eef9
XiaoXuxxxx <aonrok555@gmail.com>
2022-04-24 22:50:28 +0700

    feat: add optional request body

--- packages/backend/src/decorators/RequestDecorator.ts
+++ packages/backend/src/decorators/RequestDecorator.ts
@@ -16,6 +16,7 @@ export const RequestBody = (...keys: string[]): MethodDecorator => {
       const request: Request = args[0];
 
       if (request.body) {
+        keys = keys.filter((key) => !key.startsWith('?'));
         const hasAllRequiredBody = keys.every((key) => request.body[key]);
 
         if (hasAllRequiredBody) {
@@ -24,7 +25,7 @@ export const RequestBody = (...keys: string[]): MethodDecorator => {
       }
 
       const firstMissingKey = keys.find((key) => !request.body[key]);
-      throw new InvalidRequestException(`${firstMissingKey} does not specifed in request body`);
+      throw new InvalidRequestException(`${firstMissingKey} does not specified in request body`);
     };
   };
 };
