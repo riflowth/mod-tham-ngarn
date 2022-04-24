201a01b17419e23e07190063c373283cbfb6e7df
riflowth <riflowth@gmail.com>
2022-04-24 12:21:57 +0700

    refactor: auth service unit test

--- packages/backend/test/unit/AuthService.test.ts
+++ packages/backend/test/unit/AuthService.test.ts
@@ -1,45 +1,48 @@
-// import { LoginException } from '@/exceptions/service/LoginException';
-// import { AuthService } from '@/services/AuthService';
-// import { Cookie } from '@/utils/cookie/Cookie';
-// import { MockStaffRepository } from '@test/unit/MockStaffRepository';
+import { LoginException } from '@/exceptions/service/LoginException';
+import { AuthService } from '@/services/AuthService';
+import { Cookie } from '@/utils/cookie/Cookie';
+import { MockSessionRepository } from '@test/unit/mocks/MockSessionRepository';
+import { MockStaffRepository } from '@test/unit/mocks/MockStaffRepository';
 
-// describe('Test implementation of Auth Service', () => {
-//   let mockStaffRepository: MockStaffRepository;
-//   let authService: AuthService;
+describe('Test implementation of Auth Service', () => {
+  let mockStaffRepository: MockStaffRepository;
+  let mockSessionRepository: MockSessionRepository;
+  let authService: AuthService;
 
-//   beforeAll(async () => {
-//     mockStaffRepository = new MockStaffRepository();
-//     await mockStaffRepository.initialize();
-//     authService = new AuthService(mockStaffRepository);
-//   });
+  beforeAll(async () => {
+    mockStaffRepository = new MockStaffRepository();
+    mockSessionRepository = new MockSessionRepository();
+    await mockStaffRepository.initialize();
+    authService = new AuthService(mockStaffRepository, mockSessionRepository);
+  });
 
-//   const validUsername = '123456789';
-//   const validPassword = 'this_is_valid_password';
-//   const incorrectPassword = 'my_time_has_come';
-//   const invalidUsername = 'this_is_invalid_username';
-//   const invalidPassword = 'short';
+  const validUsername = '123456789';
+  const validPassword = 'this_is_valid_password';
+  const incorrectPassword = 'my_time_has_come';
+  const invalidUsername = 'this_is_invalid_username';
+  const invalidPassword = 'short';
 
-//   it('should throw LoginException with username requirement not match', async () => {
-//     await expect(authService.login(invalidUsername, validPassword))
-//       .rejects
-//       .toThrow(LoginException);
-//   });
+  it('should throw LoginException with username requirement not match', async () => {
+    await expect(authService.login(invalidUsername, validPassword))
+      .rejects
+      .toThrow(LoginException);
+  });
 
-//   it('should throw LoginException with password requirement not match', async () => {
-//     await expect(authService.login(validUsername, invalidPassword))
-//       .rejects
-//       .toThrow(LoginException);
-//   });
+  it('should throw LoginException with password requirement not match', async () => {
+    await expect(authService.login(validUsername, invalidPassword))
+      .rejects
+      .toThrow(LoginException);
+  });
 
-//   it('should return cookie when username and password are correct', async () => {
-//     await expect(authService.login(validUsername, validPassword))
-//       .resolves
-//       .toBeInstanceOf(Cookie);
-//   });
+  it('should return cookie when username and password are correct', async () => {
+    await expect(authService.login(validUsername, validPassword))
+      .resolves
+      .toBeInstanceOf(Cookie);
+  });
 
-//   it('should throw LoginException with username or password is incorrect', async () => {
-//     await expect(authService.login(validUsername, incorrectPassword))
-//       .rejects
-//       .toThrow(LoginException);
-//   });
-// });
+  it('should throw LoginException with username or password is incorrect', async () => {
+    await expect(authService.login(validUsername, incorrectPassword))
+      .rejects
+      .toThrow(LoginException);
+  });
+});
