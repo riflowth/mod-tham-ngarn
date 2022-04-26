1a5976b7a7e7badbeac8862e2dfae2603b3853bb
XiaoXuxxxx <aonrok555@gmail.com>
2022-04-27 02:24:14 +0700

    feat: make parameter compatible with any type

--- packages/backend/src/utils/NumberUtils.ts
+++ packages/backend/src/utils/NumberUtils.ts
@@ -4,7 +4,7 @@ export class NumberUtils {
     return Number.isInteger(number) && Number(number) > 0;
   }
 
-  public static parsePositiveInteger(number: string | number): number | null {
+  public static parsePositiveInteger(number: any): number | null {
     const parsedNumber = Number(number);
     return NumberUtils.isPositiveInteger(parsedNumber) ? parsedNumber : null;
   }
