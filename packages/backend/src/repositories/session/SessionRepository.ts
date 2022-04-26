b50a5f718a77dbd39e470db3f63be4bba87c17ba
riflowth <riflowth@gmail.com>
2022-04-27 02:26:30 +0700

    feat: add caching session implementation

--- packages/backend/src/repositories/session/SessionRepository.ts
+++ packages/backend/src/repositories/session/SessionRepository.ts
@@ -1,6 +1,11 @@
+import { RequestSession } from '@/controllers/Route';
 import { Session } from '@/entities/Session';
 import { Repository } from '@/repositories/Repository';
 
 export interface SessionRepository extends Repository<Session> {
+
+  cacheSession(sesionId: string, session: RequestSession): Promise<void>;
+
+  getCachedSession(sessionId: string): Promise<RequestSession>;
 
 }
