c72e5f56b2ddb30d84d1953319471f5bf1852b91
riflowth <riflowth@gmail.com>
2022-04-27 02:35:42 +0700

    fix: testing with new session repository interface

--- packages/backend/src/repositories/session/SessionRepository.ts
+++ packages/backend/src/repositories/session/SessionRepository.ts
@@ -4,7 +4,7 @@ import { Repository } from '@/repositories/Repository';
 
 export interface SessionRepository extends Repository<Session> {
 
-  cacheSession(sesionId: string, session: RequestSession): Promise<void>;
+  cacheSession(sessionId: string, session: RequestSession): Promise<void>;
 
   getCachedSession(sessionId: string): Promise<RequestSession>;
 
--- packages/backend/test/unit/mocks/MockSessionRepository.ts
+++ packages/backend/test/unit/mocks/MockSessionRepository.ts
@@ -1,3 +1,4 @@
+import { RequestSession } from '@/controllers/Route';
 import { Session } from '@/entities/Session';
 import { ReadOptions } from '@/repositories/ReadOptions';
 import { SessionRepository } from '@/repositories/session/SessionRepository';
@@ -5,14 +6,23 @@ import { SessionRepository } from '@/repositories/session/SessionRepository';
 export class MockSessionRepository implements SessionRepository {
 
   private readonly session: Session[] = [];
+  private readonly cache: Map<string, string> = new Map();
 
   public async create(entity: Session): Promise<Session> {
     this.session.push(entity);
     return entity;
   }
 
+  public async cacheSession(sessionId: string, session: RequestSession): Promise<void> {
+    this.cache.set(sessionId, JSON.stringify(session));
+  }
+
   public async read(entity: Session, readOptions?: ReadOptions): Promise<Session[]> {
     throw new Error('Method not implemented.');
+  }
+
+  public async getCachedSession(sessionId: string): Promise<RequestSession> {
+    return JSON.parse(this.cache.get(sessionId));
   }
 
   public async update(source: Session, destination: Session): Promise<number> {
