e8f1eb5219c2fd0d282514379ed9238f5bca774b
eltfshr <puntf2@gmail.com>
2022-04-28 12:03:50 +0700

    feat: add readByBranchId for bill repo/ fix typos

--- packages/backend/src/repositories/bill/BillRepository.ts
+++ packages/backend/src/repositories/bill/BillRepository.ts
@@ -1,6 +1,9 @@
 import { Bill } from '@/entities/Bill';
+import { ReadOptions } from '@/repositories/ReadOptions';
 import { Repository } from '@/repositories/Repository';
 
 export interface BillRepository extends Repository<Bill> {
+
+  readByBranchId(branchId: number, readOptions?: ReadOptions): Promise<Bill[]>
 
 }
--- packages/backend/src/repositories/bill/DefaultBillRepository.ts
+++ packages/backend/src/repositories/bill/DefaultBillRepository.ts
@@ -9,7 +9,7 @@ export class DefaultBillRepository extends Database implements BillRepository {
   public async create(bill: Bill): Promise<Bill> {
     const parameter = {
       store_name: bill.getStoreName(),
-      bill_date: DateUtil.formatToSQL(bill.getOrderDate()),
+      order_date: DateUtil.formatToSQL(bill.getOrderDate()),
       order_by: bill.getOrderBy(),
     };
 
@@ -25,7 +25,7 @@ export class DefaultBillRepository extends Database implements BillRepository {
     const parameter = {
       bill_id: bill.getBillId(),
       store_name: bill.getStoreName(),
-      bill_date: DateUtil.formatToSQL(bill.getOrderDate()),
+      order_date: DateUtil.formatToSQL(bill.getOrderDate()),
       order_by: bill.getOrderBy(),
     };
 
@@ -35,7 +35,7 @@ export class DefaultBillRepository extends Database implements BillRepository {
       return new Bill()
         .setBillId(result.bill_id)
         .setStoreName(result.store_name)
-        .setOrderDate(DateUtil.formatFromSQL(result.bill_date))
+        .setOrderDate(DateUtil.formatFromSQL(result.order_date))
         .setOrderBy(result.order_by);
     });
 
@@ -45,14 +45,14 @@ export class DefaultBillRepository extends Database implements BillRepository {
   public async update(source: Bill, destination: Bill): Promise<number> {
     const sourceParameter = {
       store_name: source.getStoreName(),
-      bill_date: DateUtil.formatToSQL(source.getOrderDate()),
+      order_date: DateUtil.formatToSQL(source.getOrderDate()),
       order_by: source.getOrderBy(),
     };
 
     const destinationParameter = {
       bill_id: source.getBillId(),
       store_name: destination.getStoreName(),
-      bill_date: DateUtil.formatToSQL(destination.getOrderDate()),
+      order_date: DateUtil.formatToSQL(destination.getOrderDate()),
       order_by: destination.getOrderBy(),
     };
 
@@ -65,13 +65,50 @@ export class DefaultBillRepository extends Database implements BillRepository {
     const parameter = {
       bill_id: bill.getBillId(),
       store_name: bill.getStoreName(),
-      bill_date: DateUtil.formatToSQL(bill.getOrderDate()),
+      order_date: DateUtil.formatToSQL(bill.getOrderDate()),
       order_by: bill.getOrderBy(),
     };
 
     const result: any = await this.getSqlBuilder().delete('Bill', parameter);
 
     return result[0].affectedRows;
+  }
+
+  public async readByBranchId(branchId: number, readOptions?: ReadOptions): Promise<Bill[]> {
+    const { limit, offset } = readOptions || {};
+
+    if (limit || offset) {
+      const isIntegerOptions = Number.isInteger(limit) || Number.isInteger(offset);
+      if (!isIntegerOptions) {
+        throw new Error('limit and offset must be integer');
+      }
+
+      const isValidReadOptions = limit > 0 && (!offset || offset >= 0);
+      if (!isValidReadOptions) {
+        throw new Error('Invalid relationship limit or offset');
+      }
+    }
+
+    const limitOption = (limit && limit >= 0) && `LIMIT ${limit}`;
+    const offsetOption = (limitOption && offset > 0) && `OFFSET ${offset}`;
+
+    const query = [
+      'SELECT * FROM Bill WHERE order_by IN (SELECT staff_id FROM Staff WHERE branch_id = ?)',
+      limitOption,
+      offsetOption,
+    ].filter(Boolean).join(' ');
+
+    const results: any = await this.execute(query, [branchId]);
+
+    const bills = results[0].map((result) => {
+      return new Bill()
+        .setBillId(result.bill_id)
+        .setStoreName(result.store_name)
+        .setOrderDate(DateUtil.formatFromSQL(result.order_date))
+        .setOrderBy(result.order_by);
+    });
+
+    return bills;
   }
 
 }
