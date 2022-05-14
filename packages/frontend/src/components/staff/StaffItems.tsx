d9ff0df89c07a93a71ab95714a1f4d2670acd4c7
Porping <porsteam@hotmail.com>
2022-05-14 16:33:41 +0700

    feat:add drop down in staffs table

--- packages/frontend/src/components/staff/StaffItems.tsx
+++ packages/frontend/src/components/staff/StaffItems.tsx
@@ -1,4 +1,4 @@
-import { Disclosure, Transition } from '@headlessui/react';
+import { Disclosure, Transition } from "@headlessui/react";
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -18,37 +18,43 @@ import LastPageIcon from "@mui/icons-material/LastPage";
 import { TableHead } from "@mui/material";
 import { MyDialog } from "@components/MyDiaLog";
 import axios, { AxiosResponse } from "axios";
-import { TableColumms } from '@components/table/TableColumns';
-import { Entity } from '@models/Entity';
-import { Staff } from '@models/Staff';
-import { ChevronDoubleRightIcon } from '@heroicons/react/solid';
-import { ChevronDoubleDownIcon } from '@heroicons/react/solid';
+import { TableColumms } from "@components/table/TableColumns";
+import { Entity } from "@models/Entity";
+import { Staff } from "@models/Staff";
+import { ChevronDoubleRightIcon } from "@heroicons/react/solid";
+import { ChevronDoubleDownIcon } from "@heroicons/react/solid";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Typography from "@mui/material/Typography";
+import Collapse from "@mui/material/Collapse";
 
-export const StaffItems = ({ rows }: { rows: Array<Staff> }) => {
-  const rowElements = rows.map((row, i) => {
-    return (
-      <TableRow key={row.staffId}>
-        {/* <Disclosure>
-          {({ open }) => (
-          <>
-            <Disclosure.Button>
-              <div className="w-4">
-                <ChevronDoubleRightIcon
-                  className={`${open ? "transform rotate-90" : ""}`}
-                />
-              </div>
-            </Disclosure.Button>
+function Row(props: { row: Staff }) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
 
-            <Disclosure.Panel>{"hello"}</Disclosure.Panel>
-          </>
-          )}
-        </Disclosure> */}
-        <TableCell key={i} style={{ width: 160 }}>{row.staffId}</TableCell>
-        <TableCell key={i} style={{ width: 160 }}>{row.fullName.split(' ')[0]}</TableCell>
-        <TableCell key={i} style={{ width: 160 }}>{row.fullName.split(' ')[1]}</TableCell>
-        <TableCell key={i} style={{ width: 160 }}>{row.branchId}</TableCell>
-        <TableCell key={i} style={{ width: 160 }}>{row.zoneId}</TableCell>
-        <TableCell key={i} style={{ width: 160 }}>{row.position}</TableCell>
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell style={{ width: 160 }}>{row.staffId}</TableCell>
+        <TableCell style={{ width: 160 }}>
+          {row.fullName.split(" ")[0]}
+        </TableCell>
+        <TableCell style={{ width: 160 }}>
+          {row.fullName.split(" ")[1]}
+        </TableCell>
+        <TableCell style={{ width: 160 }}>{row.branchId}</TableCell>
+        <TableCell style={{ width: 160 }}>{row.zoneId}</TableCell>
+        <TableCell style={{ width: 160 }}>{row.position}</TableCell>
+
         <div className="space-x-3">
           <button className="p-2 font-semibold bg-green-400 rounded-md">
             Edit
@@ -58,12 +64,34 @@ export const StaffItems = ({ rows }: { rows: Array<Staff> }) => {
           </button>
         </div>
       </TableRow>
-    );
-  })
-  return (
-    <>
-      {rowElements}
-    </>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>{/* เนิ้อหา */}</TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
   );
 }
 
+export const StaffItems = ({ rows }: { rows: Array<Staff> }) => {
+  const [open, setOpen] = React.useState(false);
+  const rowElements = rows.map((row, i) => <Row key={i} row={row} />);
+  return <>{rowElements}</>;
+};
