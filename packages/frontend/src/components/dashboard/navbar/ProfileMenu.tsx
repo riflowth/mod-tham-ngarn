d0c7e4df27d4ddfa364bfd885db67f6b8b6b4a92
riflowth <riflowth@gmail.com>
2022-05-15 02:43:06 +0700

    fix: use real user data

--- packages/frontend/src/components/dashboard/navbar/ProfileMenu.tsx
+++ packages/frontend/src/components/dashboard/navbar/ProfileMenu.tsx
@@ -12,7 +12,7 @@ type ProfileMenuProp = {
 export const ProfileMenu = ({
   id,
 }: ProfileMenuProp) => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
 
   return (
     <Menu as="div" className="relative">
@@ -36,8 +36,8 @@ export const ProfileMenu = ({
         <Menu.Items className="absolute z-10 right-0 w-48 mt-2 origin-top rounded-md text-white bg-zinc-800 border border-zinc-600 shadow-md focus:outline-none divide-y divide-zinc-600">
           <div className="px-1 py-0">
             <Menu.Item as="div" className="p-2 flex flex-col">
-              <span className="text-sm text-violet-400 font-semibold">Hi, Nathee</span>
-              <span className="text-xs text-zinc-400 font-light">CEO</span>
+              <span className="text-sm text-violet-400 font-semibold">Hi, {user!.name}</span>
+              <span className="text-xs text-zinc-400 font-light">{user!.role}</span>
             </Menu.Item>
           </div>
           <div className="px-1 py-1">
