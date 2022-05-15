d32bb0128033e306616ddcb14f40f8a6a21856f1
eltfshr <puntf2@gmail.com>
2022-05-16 01:19:35 +0700

    fix: remove input from dependency array

--- packages/frontend/src/components/machinepart/MachinePartModal.tsx
+++ packages/frontend/src/components/machinepart/MachinePartModal.tsx
@@ -57,7 +57,7 @@ export const MachinePartsModal = ({
     };
 
     submit();
-  }, [confirm, input]);
+  }, [confirm]);
 
   return (
     <div className="space-y-2 text-white">
--- packages/frontend/src/components/maintenancelog/MaintenanceLogModal.tsx
+++ packages/frontend/src/components/maintenancelog/MaintenanceLogModal.tsx
@@ -59,7 +59,7 @@ export const MaintenanceLogModal = ({ confirm, current, action }: MaintenanceLog
     };
 
     submit();
-  }, [confirm, input]);
+  }, [confirm]);
 
   return (
     <div className="space-y-2 text-white">
