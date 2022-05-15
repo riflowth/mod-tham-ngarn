9c720a068eca7a59c37d6bd49cbb43ef1a4d5251
riflowth <riflowth@gmail.com>
2022-05-16 05:34:21 +0700

    fix: why github secret not working, I want to sleep bro!!!

--- packages/frontend/src/utils/Fetch.ts
+++ packages/frontend/src/utils/Fetch.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_WEB_URL || 'http://localhost:4000'
+// const BASE_URL = process.env.NEXT_PUBLIC_WEB_URL || 'http://localhost:4000'
+
+const BASE_URL = 'https://cpe231.riflowth.com/api';
 
 const fetch = axios.create({
   baseURL: BASE_URL,
